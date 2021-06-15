73011fdd7373a7da12c946434fd26075dd74492b
Christopher Grady <grittygrady@gmail.com>
2021-06-15 13:00:17 -0700

    landing page pretty complete

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import BGImg from '../images/bwp-wall.jpeg';
 
 const Home = () => {
   return (
-    <div>
-      <h1>Home Page!</h1>
-    </div>
+    <main>
+      <img
+        src={BGImg}
+        alt='brick wall with black white and pink paint'
+        className='absolute object-cover w-full h-full'
+      />
+      <section className='relative flex justify-center lg:justify-start min-h-screen pt-12 pl-4 lg:pl-24 pr-16 decorative'>
+        <h1 className='text-5xl lg:text-7xl text-pink-500 font-bold leading-none md:leading-snug bg-gray-900 h-28 md:h-20 rounded w-48 lg:w-64 lg:h-60'>
+          Oh mY BLoG!
+        </h1>
+      </section>
+    </main>
   );
 };
 
