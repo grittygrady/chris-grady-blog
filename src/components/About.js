ae7f258c7614647fcc68ddb5accdcf835d0950f2
Christopher Grady <grittygrady@gmail.com>
2021-06-22 11:23:09 -0700

    working on about page

--- src/components/About.js
+++ src/components/About.js
@@ -9,10 +9,20 @@ const About = () => {
         alt='Drowning'
         className='absolute object-cover w-full h-full'
       />
-      <section className='relative flex justify-center lg:justify-start min-h-screen pt-12 pl-4 lg:pl-24 pr-16 decorative'>
-        <h1 className='text-5xl lg:text-7xl text-pink-500 font-bold leading-none md:leading-snug bg-gray-900 h-28 md:h-20 rounded w-48 lg:w-64 lg:h-60 decorative'>
-          Oh mY BLoG!
-        </h1>
+      <section className='relative flex justify-center lg:justify-start  pt-12 pl-4 lg:pl-24 pr-16 decorative mx-auto'>
+        <div className='bg-gray-900 bg-opacity-80 md:bg-opacity-100 flex flex-col'>
+          <h1 className='text-3xl md:text-5xl lg:text-7xl text-blue-400 leading-none md:leading-snug   md:h-20 rounded  lg:w-64 lg:h-60 decorative'>
+            Hi, I'm <span className='text-pink-500 inline'>Grady</span>
+          </h1>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quos
+            dolore nam eos id, expedita iusto hic nisi minima. Dicta facilis
+            tempore alias est debitis ut molestiae, nulla dignissimos labore
+            tempora sed! Velit qui laborum deleniti maiores fuga distinctio!
+            Praesentium quia mollitia dolor ratione molestiae possimus suscipit
+            laudantium repudiandae fugiat ut!
+          </p>
+        </div>
       </section>
     </main>
   );
