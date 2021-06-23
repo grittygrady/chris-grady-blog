ad58ed78b9818279e6b34784993fe3707f4f1428
Christopher Grady <grittygrady@gmail.com>
2021-06-23 10:42:29 -0700

    customizing tailwind theme, about page nearly done

--- src/components/About.js
+++ src/components/About.js
@@ -1,30 +1,30 @@
-import profilePic from '../images/profilecandyskull.jpg';
-import drowningBackground from '../images/drowning.jpg';
-
 const About = () => {
   return (
-    <main>
-      <img
-        src={drowningBackground}
-        alt='Drowning'
-        className='absolute object-cover w-full h-full'
-      />
-      <section className='relative flex justify-center lg:justify-start  pt-12 pl-4 lg:pl-24 pr-16 decorative mx-auto'>
-        <div className='bg-gray-900 bg-opacity-80 md:bg-opacity-100 flex flex-col'>
-          <h1 className='text-3xl md:text-5xl lg:text-7xl text-blue-400 leading-none md:leading-snug   md:h-20 rounded  lg:w-64 lg:h-60 decorative'>
-            Hi, I'm <span className='text-pink-500 inline'>Grady</span>
-          </h1>
-          <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quos
-            dolore nam eos id, expedita iusto hic nisi minima. Dicta facilis
-            tempore alias est debitis ut molestiae, nulla dignissimos labore
-            tempora sed! Velit qui laborum deleniti maiores fuga distinctio!
-            Praesentium quia mollitia dolor ratione molestiae possimus suscipit
-            laudantium repudiandae fugiat ut!
-          </p>
+    <div className='bg-drowningPic bg-center md:min-h-screen bg-cover py-16 '>
+      <section className='container px-2 mx-auto flex justify-end '>
+        <div className='bg-pink-500 bg-opacity-75 rounded p-2 '>
+          <div className='flex flex-col '>
+            <h1 className='decorative bg-gray-900 bg-opacity-90 rounded-lg text-blue-400 text-3xl text-center p-3 md:text-6xl'>
+              Chris <span className='decorative text-pink-600'>GraDy</span>
+            </h1>
+            <p className='text-gray-900 p-6 text-lg'>
+              90s obsessed. Retro gaming. Punk rock. Jazz. Coding. Guitar.
+              Sampling. Howling disguised as "singing". Vices include but are
+              not limited to:{' '}
+            </p>
+            <ul className='ml-2  text-md px-8'>
+              <li className='md:py-3'>A case of sparkling water a day.</li>
+              <li className='md:py-3'>The Simpsons. Even the bad episodes.</li>
+              <li className='md:py-3'>Raspberry Pi.</li>
+              <li className='md:py-3'>
+                Graphic novels - don't you{' '}
+                <span className='font-bold'>dare</span> call them comic books
+              </li>
+            </ul>
+          </div>
         </div>
       </section>
-    </main>
+    </div>
   );
 };
 
